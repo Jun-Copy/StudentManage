82d215c977abc330a138e3ef0b02d3dd86809ef5
Jun <Jun@qq.com>
2019-04-02 20:35:55 +0800

    添加了公共page和result以及result工具-2

--- src/main/java/com/san/stum/utils/ResultUtil.java
+++ src/main/java/com/san/stum/utils/ResultUtil.java
@@ -1,12 +1,10 @@
 package com.san.stum.utils;
 
-import com.wtj.oa.entity.SysResult;
+import com.san.stum.common.SysResult;
 
 public class ResultUtil {
 	/**
 	 * 返回是否成功的结果类
-	 * @param result
-	 * @return
 	 */
 	public static SysResult getSysResult(int result) {
 		SysResult sysResult = new SysResult();
